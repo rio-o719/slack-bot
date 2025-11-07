8363f7c2c0cc7cde5214ee4aac486fd27fb94e61
rio-o719 <rio.s.0719@icloud.com>
2025-11-07 17:44:50 +0900

    Initial commit of the Python Slack bot

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# ボットトークンを渡してアプリを初期化します
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# 'こんにちは' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://docs.slack.dev/tools/bolt-python/reference/kwargs_injection/args.html
+@app.message("こんにちは")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"こんにちは、<@{message['user']}> さん！")
+
+if __name__ == "__main__":
+    # アプリを起動して、ソケットモードで Slack に接続します
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
